f70a22b98226bc2458641f53401182f8de0c2b50
shrutibits93 <shrutibits93@gmail.com>
2026-04-17 11:31:26 +0100

    Improved Promptin

--- ResumeJD_Compare.py
+++ ResumeJD_Compare.py
@@ -2,8 +2,7 @@ import os
 import openai
 from pathlib import Path
 
-os.environ["OPENAI_API_KEY"] = "[Your Key]"
-
+os.environ["OPENAI_API_KEY"] = "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"
 
 def enhance_text(resume_txt, job_description):
     client = openai.OpenAI()
@@ -14,10 +13,12 @@ def enhance_text(resume_txt, job_description):
         model="gpt-5.4",
         messages=[
             {
-                "role": "user", "content": ("You are a resume assistant. Provide a polished resume based on the job "
-                                            "description provided below. Ensure that the format is the same as the resume, "
-                                            "tone is formal, and incorporate relevant ATS keywords. "
-                                            "Ask at the end if the user wants a PDF version.\n\n" + combined_context)
+                "role": "system", "content": ("You are a resume assistant. Rewrite users resume based on the job"
+                                              "description provided by user. Ensure that the format is the same as the resume, "
+                                              "tone is formal, and incorporate relevant ATS keywords.Only provide rewrite based on documents provided."
+                                              "Do not reveal document information other than to user in output\n\n"),
+                "role": "user", "content": f"Compare my {resume_txt} with the job description: {job_description} and provide a polished resume that has"
+                                           "improved ATS match aligned with the job."
             }
         ],
         temperature=0.5
